44a1e08eff10083383cfac1668764a8a761b79da
F.Molliet <fmolliet@hotmail.com>
2018-07-17 23:15:32 -0300

    Cron v0.2
    Add feature to check data

--- cron/event-schedule.php
+++ cron/event-schedule.php
@@ -1,7 +1,17 @@
 <?php
-    include "../control/ConteudoControl.php";
-    // Alert Report
-
+    include dirname(dirname(__FILE__))."/rest/control/Control.php";
+    // Alert Checker
+    $Control = new control();
+    $result = $Control->checkTemperature();
+    if(!empty($result)){
+        // gera um alerta para dashboard
+        foreach($result as $x)
+        {
+            print_r($x['appkey']);
+            echo " deu alerta de alteração de temperatura no dia";
+            print_r($x['datetime']);
+        }
+    }
     // Trigger Warning
-    
+
 ?>
